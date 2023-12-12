64a8c77a23640dae01b81c00bdeea787bf04024f
kamicup <yoshikazu.hashimoto@gmail.com>
2023-12-12 16:11:12 +0900

    AWS SAM ざっくり

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import {Configuration, IgnorePlugin} from 'webpack'
+
+const config: Configuration = {
+    target: 'node',
+    optimization: {
+        minimize: false,
+    },
+    entry: {
+        track: {
+            import: './src/handlers/track.ts',
+            filename: "track/index.js",
+        },
+    },
+    output: {
+        path: `${__dirname}/dist`,
+        libraryTarget: 'commonjs2',
+    },
+    externals: ['aws-sdk'],
+    module: {
+        rules: [
+            { test: /\.ts$/, use: [ { loader: 'ts-loader' } ]}
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts'],
+    },
+    plugins: [
+        new IgnorePlugin({
+            resourceRegExp: /^cardinal$/,
+            contextRegExp: /./,
+        }),
+    ]
+};
+
+export default config;
