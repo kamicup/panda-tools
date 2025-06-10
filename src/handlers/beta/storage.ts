9d57222b9f3d84573969536fcc375c336947e91c
kamicup <yoshikazu.hashimoto@gmail.com>
2025-06-10 18:05:53 +0900

    単純なストレージ機能の追加

---
+++ src/handlers/beta/storage.ts
@@ -0,0 +1,68 @@
+import {APIGatewayProxyEventV2} from "aws-lambda";
+import {callExternalResponse, storageTableName, region} from "./lib/env";
+import {
+    DynamoDBClient,
+    PutItemCommand,
+    GetItemCommand
+} from "@aws-sdk/client-dynamodb";
+
+const META_PUT = 'PUT'
+const META_GET = 'GET'
+
+type PutData = {
+    key: string
+    value: string
+}
+type GetData = {
+    key: string
+}
+
+export default async function storage(event: APIGatewayProxyEventV2, data: any) {
+
+    if (data[META_PUT]) {
+        return await processPut(data[META_PUT] as PutData)
+    }
+
+    if (data[META_GET]) {
+        return await processGet(data[META_GET] as GetData)
+    }
+
+    return callExternalResponse(200, JSON.stringify({
+        result: false,
+    }))
+}
+
+function newClient() {
+    return new DynamoDBClient({
+        region: region,
+    })
+}
+
+async function processPut(data: PutData) {
+    const _ = await newClient().send(new PutItemCommand({
+        TableName: storageTableName,
+        Item: {
+            Key: {S: data.key},
+            Value: {S: data.value},
+        },
+    }))
+    return callExternalResponse(200, JSON.stringify({result: true}))
+}
+
+async function processGet(data: GetData) {
+    const output = await newClient().send(new GetItemCommand({
+        TableName: storageTableName,
+        Key: {
+            Key: {S: data.key},
+        },
+    }))
+    if (output.Item && output.Item.Value && output.Item.Value.S) {
+        const value = output.Item.Value.S
+        return callExternalResponse(200, JSON.stringify({
+            result: true,
+            key: data.key,
+            value: value,
+        }))
+    }
+    return callExternalResponse(200, JSON.stringify({result: false}))
+}
